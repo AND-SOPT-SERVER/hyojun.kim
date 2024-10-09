7e214a0e8aefaee65fd8d36daf6ed913fdc885f0
Khyojun <khyojun1104@gmail.com>
2024-10-09 19:23:53 +0900

    feat : 영구 저장(file/IO) 방식과 기존 방식을 분리하기 위해 interface 활용하여 역할 분리

---
+++ src/main/java/org/sopt/seminar1/DiaryFileRepository.java
@@ -0,0 +1,94 @@
+package org.sopt.seminar1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+import org.sopt.seminar1.Main.UI.InvalidInputException;
+
+public class DiaryFileRepository implements DiaryRepository {
+
+    private static final int ID_INCREMENT_VALUE = 1;
+    private final AtomicLong numbering = new AtomicLong();
+    private final DiaryFileAccessor diaryFileAccessor = new DiaryFileAccessor();
+
+    @Override
+    public void save(String body) {
+        ConcurrentMap<Long, Diary> storage = diaryFileAccessor.readDiary();
+        final long id = numbering.addAndGet(ID_INCREMENT_VALUE);
+        storage.put(id, new Diary(id, body));
+        List<Diary> diaryList = convertMapToList(storage);
+        diaryFileAccessor.writeDiary(diaryList);
+    }
+
+    @Override
+    public List<Diary> findAll() {
+        List<Diary> diaryList = new ArrayList<>();
+        ConcurrentMap<Long, Diary> storage = diaryFileAccessor.readDiary();
+
+        for (long index = 1; index <= numbering.longValue(); index++) {
+            if (Objects.isNull(storage.get(index))) {
+                continue;
+            }
+
+            diaryList.add(storage.get(index));
+        }
+
+        return diaryList;
+    }
+
+    @Override
+    public void patch(Long id, String body) {
+        ConcurrentMap<Long, Diary> storage = diaryFileAccessor.readDiary();
+
+        isPresentDiary(storage, id);
+        Diary diary = storage.get(id);
+        diary.setBody(body);
+        storage.put(id, diary);
+        List<Diary> diaryList = convertMapToList(storage);
+
+        diaryFileAccessor.writeDiary(diaryList);
+    }
+
+
+    @Override
+    public void restore() {
+        ConcurrentMap<Long, Diary> storage = diaryFileAccessor.readDiary();
+        List<Diary> trashStorage = diaryFileAccessor.trashRead();
+        for(int i=0; i<trashStorage.size(); i++){
+            Diary restoreDiary = trashStorage.get(i);
+            Long id = trashStorage.get(i).getId();
+            storage.putIfAbsent(id, restoreDiary);
+        }
+        trashStorage.clear();
+
+        diaryFileAccessor.clearTrash();
+        diaryFileAccessor.writeDiary(convertMapToList(storage));
+    }
+
+    @Override
+    public void delete(Long id) {
+        ConcurrentMap<Long, Diary> storage = diaryFileAccessor.readDiary();
+        List<Diary> trashStorage = diaryFileAccessor.trashRead();
+        isPresentDiary(storage, id);
+        trashStorage.add(storage.get(id));
+        storage.remove(id, storage.get(id));
+        diaryFileAccessor.writeDiary(convertMapToList(storage));
+        diaryFileAccessor.trashWrite(trashStorage);
+    }
+
+    private void isPresentDiary(Map<Long, Diary> storage, Long id) {
+        if (storage.get(id) == null) {
+            throw new InvalidInputException();
+        }
+    }
+
+
+    private List<Diary> convertMapToList(ConcurrentMap<Long, Diary> storage) {
+        return storage.entrySet().stream().sorted(Entry.comparingByKey())
+            .map(Entry::getValue).toList();
+    }
+}
--- src/main/java/org/sopt/seminar1/DiaryRepository.java
+++ src/main/java/org/sopt/seminar1/DiaryRepository.java
@@ -1,67 +1,16 @@
 package org.sopt.seminar1;
 
-import org.sopt.seminar1.Main.UI.InvalidInputException;
+import java.util.List;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
+public interface DiaryRepository {
+    void save(final String body);
 
-public class DiaryRepository {
-    private static final int ID_INCREMENT_VALUE =1;
-    private final Map<Long, Diary> storage = new ConcurrentHashMap<>();
-    private final AtomicLong numbering = new AtomicLong();
-    private static List<Diary> trashStorage = new ArrayList<>();
+    List<Diary> findAll();
 
+    void patch(final Long id, final String body);
 
+    void restore();
 
-    void save(final String body){
-        final long id = numbering.addAndGet(ID_INCREMENT_VALUE);
-        storage.put(id, new Diary(id, body));
-    }
-
-    List<Diary> findAll(){
-        List<Diary> diaryList = new ArrayList<>();
-
-        for(long index =1; index<=numbering.longValue(); index++){
-            if(Objects.isNull(storage.get(index)))
-                continue;
-
-            diaryList.add(storage.get(index));
-        }
-
-        return diaryList;
-    }
-
-    void patch(Long id, String body){
-        isPresentDiary(id);
-        Diary diary = storage.get(id);
-        diary.setBody(body);
-        storage.put(id, diary);
-    }
-
-    void delete(Long id)
-    {
-        isPresentDiary(id);
-        trashStorage.add(storage.get(id));
-        storage.remove(id, storage.get(id));
-    }
-
-    void restore(){
-        for(int i=0; i<trashStorage.size(); i++){
-            Diary restoreDiary = trashStorage.get(i);
-            Long id = trashStorage.get(i).getId();
-            storage.putIfAbsent(id, restoreDiary);
-        }
-        trashStorage.clear();
-    }
-
-
-    private void isPresentDiary(Long id) {
-        if(storage.get(id)==null)
-            throw new InvalidInputException();
-    }
-
-
-
+    void delete(Long id);
 
 }
