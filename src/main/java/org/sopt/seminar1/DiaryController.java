cad1689ff932bf1f11de99b711a3e0ea5c324d5d
Khyojun <nandong5923@naver.com>
2024-10-10 18:37:52 +0900

    refactor: controller에서의 예외 처리 Parser, Validator에게 넘겨서 진행
    - 역할 분배를 위해 분리작업 진행
    - parser : parse 를 위한 역할
    - validator : 입력값 검증을 위한 역할

--- src/main/java/org/sopt/seminar1/DiaryController.java
+++ src/main/java/org/sopt/seminar1/DiaryController.java
@@ -3,15 +3,10 @@ package org.sopt.seminar1;
 import org.sopt.seminar1.Main.UI.InvalidInputException;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class DiaryController {
-    private static final int MAX_LENGTH=30;
     private Status status = Status.READY;
     private final DiaryService diaryService = new DiaryService();
-    private final Pattern graphemePattern = Pattern.compile("\\X");
-    private final Matcher graphemeMatcher = graphemePattern.matcher("");
 
     Status getStatus() {
         return status;
@@ -31,54 +26,23 @@ public class DiaryController {
     }
 
     final void post(final String body) {
-        validateBody(body);
+        Validator.validateBody(body);
         diaryService.save(body);
     }
 
     final void delete(final String id) {
-        try {
-            String trimmedInputId = id.trim();
-            diaryService.delete(Long.parseLong(trimmedInputId));
-        }catch (NumberFormatException e){
-            throw new InvalidInputException();
-        }
+        String trimmedInputId = id.trim();
+        diaryService.delete(Parser.stringToLong(trimmedInputId));
     }
 
     final void patch(final String id, final String body) {
-        try {
-            validateBody(body);
-            String trimmedInputId = id.trim();
-            diaryService.patch(Long.parseLong(trimmedInputId), body);
-        }catch (NumberFormatException e){
-            throw new InvalidInputException();
-        }
+        Validator.validateBody(body);
+        String trimmedInputId = id.trim();
+        diaryService.patch(Parser.stringToLong(trimmedInputId), body);
     }
 
-    final void restore(){
+    final void restore() {
         diaryService.restore();
-    }
-
-
-    private void validateBody(String body) {
-        if(body.length() > MAX_LENGTH)
-            throw new InvalidInputException();
-        if(body.trim().isBlank())
-            throw new InvalidInputException();
-        if(countGraphemes(body) > MAX_LENGTH)
-            throw new InvalidInputException();
-    }
-
-    private int countGraphemes(String body) {
-        if (body == null) {
-            return 0;
-        }
-        graphemeMatcher.reset(body);
-        int count = 0;
-        while (graphemeMatcher.find()) {
-            count++;
-        }
-        System.out.println("count는 : " + count);
-        return count;
     }
 
 
